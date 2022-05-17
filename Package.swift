689208b11ad09bcef56cad7b4121c132e5a45e13
awxkee <qnljoy@gmail.com>
2022-05-18 00:16:25 +0200

    initial release

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "libwebp",
-            targets: ["libwebp", "libwebp"]),
+            targets: ["libwebp", "libwebpmux"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
