7f5bdcf18877168b3ae39fb55773f438829f9194
awxkee <qnljoy@gmail.com>
2022-05-20 21:25:02 +0200

    macos catalyst + unsafe buffers

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "libwebp",
     platforms: [
-        .macOS(.v10_10), .iOS(.v13)
+        .macOS(.v10_10), .iOS(.v13), .macCatalyst(.v14)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
